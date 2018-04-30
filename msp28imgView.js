881099b93a866d65d3e6a7fc731ff89a726bcfe3
MathiasGriesang <38847020+MathiasGriesang@users.noreply.github.com>
2018-04-30 10:41:35 -0300

    Update msp28imgView.js

--- msp28imgView.js
+++ msp28imgView.js
@@ -1,9 +1,13 @@
+var is_open=false;
+var atual_id=1;
 function search(id,q){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 		 if(q==1){$("#imgAndArrows").html(this.responseText);} else if(q==2){$("#showText").html(this.responseText);}
 		 componentHandler.upgradeDom();
+		 is_open=true;
+		 atual_id=id;
 		}
 	};
 	xhttp.open("GET", "plugin/msp28imgView.php?id="+id+"&q="+q, true);
@@ -38,7 +42,26 @@ function back_midia(id){
 }
 function close_midia(){
 	$('#msp28imgView').hide();
+	is_open=false;
 	$("#loadX").show();
 	$("#container2").hide();
 	$("#closeX").hide();
 }
+
+$(document).ready(function(){
+  $(document).keypress(function(e){
+  	if (is_open) {
+		if(e.wich == 37 || e.keyCode == 37){
+			if ($("div#leftBack").length) { 
+				back_midia(atual_id);
+			}
+		}else if(e.wich == 39 || e.keyCode == 39){
+			if ($("div#rightNext").length) { 
+				next_midia(atual_id);
+			}
+		}else if(e.wich == 27 || e.keyCode == 27){
+			close_midia();
+		}
+	}
+  })
+})
